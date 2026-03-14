a3f11b18d6b8da17797298c885ad11c54f567072
Katie Silva <kts03290@gmail.com>
2026-03-14 01:06:37 +0100

    removed duplicates

--- QuickFix.py
+++ QuickFix.py
@@ -1,11 +1,26 @@
 
 import os
-import easygui
 def main():
-    filepath = easygui.fileopenbox()
+    for dirsRoot, dirs, _ in os.walk("./Pictures"):
+        for dir in dirs:
+            images = []
+            for root, _, files in os.walk(os.path.join(dirsRoot, dir)):
+                for file in files:
+                    if not imageInArr(os.path.join(root, file), images):
+                        images.append(os.path.join(root, file))
+                    else:
+                        print("Deleting "+os.path.join(root, file))
+                        os.remove(os.path.join(root, file))
+
+                        
 
 
 
+def imageInArr(img, arr):
+    return True in [compareImages(img, x) for x in arr ]
+
+def compareImages(a, b):
+    return os.path.getsize(a) == os.path.getsize(b)
         
 
 
